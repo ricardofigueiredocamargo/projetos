4d3514146d26833eff56c8b499cb98249e7dfa4e
ricardofigueiredocamargo <116184098+ricardofigueiredocamargo@users.noreply.github.com>
2023-04-23 15:13:17 -0300

    Projeto Orçamento Pessoal

--- orcamento-pessoal/script/app.js
+++ orcamento-pessoal/script/app.js
@@ -44,6 +44,10 @@ class Bd {
             }
         }
     }
+
+    removerItem(id) {
+        localStorage.removeItem(id)
+    }
 }
 
 class Modal {
@@ -57,7 +61,7 @@ class Modal {
         this.titulo.classList.add('text-danger')
         this.botao.classList.add('btn-danger')
 
-        this.titulo.innerHTML = `Erro na gravação`
+        this.titulo.innerHTML = `Erro`
         this.desricao.innerHTML = `Existem campos obrigatórios que não foram preenchidos!`
         this.botao.innerHTML = `Voltar e corrigir`
     }
@@ -72,6 +76,16 @@ class Modal {
         this.botao.innerHTML = `Voltar`
     }
 
+    removerDespesa() {
+        this.titulo.classList.add('text-warning')
+        this.botao.classList.add('btn-warning')
+        this.botao.addEventListener('click', recarregarPagina)
+
+        this.titulo.innerHTML = `Despesa removida`
+        this.desricao.innerHTML = `A sua despesa foi removida com sucesso!`
+        this.botao.innerHTML = `Voltar`
+    }
+
     removerClasses() {
         if (this.titulo.classList.contains('text-danger')) {
             this.titulo.classList.remove('text-danger')
@@ -82,6 +96,12 @@ class Modal {
             this.titulo.classList.remove('text-success')
             this.botao.classList.remove('btn-success')
             this.botao.removeEventListener('click', limparDados)
+        }
+
+        if (this.titulo.classList.contains('text-warning')) {
+            this.titulo.classList.remove('text-warning')
+            this.botao.classList.remove('btn-warning')
+            this.botao.removeEventListener('click', recarregarPagina)
         }
     }
 }
@@ -94,6 +114,14 @@ let modal = new Modal()
 let todasDespesas = []
 
 let listaDespesas = document.querySelector('#lista-despesas')
+
+let valorTotal = document.querySelector('#valor-total')
+
+let resultadoPesquisaValor = document.querySelector('#resultado-pesquisa-valor')
+
+let filtroSelecionado = document.querySelector('#filtro-selecionado')
+
+let valorGasto = document.querySelector('#valor-gasto')
 
 // FUNÇÕES PRINCIPAIS -----------------------------------
 function registrarDespesa() {
@@ -131,27 +159,105 @@ function mostrarListaDespesas() {
     carregaListaDespesas(todasDespesas)
 }
 
+function pesquisarDespesa() {
+    let ano = document.querySelector('#iano').value
+    let mes = document.querySelector('#imes').value
+    let dia = document.querySelector('#idia').value
+    let tipo = document.querySelector('#itipo').value
+    let descricao = document.querySelector('#idescricao').value
+    let valor = document.querySelector('#ivalor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    let despesaFiltrada = filtrar(despesa)
+
+    listaDespesas.innerHTML = ''
+
+    carregaListaDespesas(despesaFiltrada)
+}
+
 function carregaListaDespesas(lista) {
     lista.forEach((d) => {
-        d.dia = ajustarDiaMes(d.dia)
-        d.mes = ajustarDiaMes(d.mes)
-        d.tipo = ajustarTipo(d.tipo)
-        d.valor = ajustarValor(d.valor)
+        let dia = ajustarDiaMes(d.dia)
+        let mes = ajustarDiaMes(d.mes)
+        let tipo = ajustarTipo(d.tipo)
+        let valor = ajustarValor(d.valor)
 
         let linha = listaDespesas.insertRow()
-        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
-        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(0).innerHTML = `${dia}/${mes}/${d.ano}`
+        linha.insertCell(1).innerHTML = tipo
         linha.insertCell(2).innerHTML = d.descricao
-        linha.insertCell(3).innerHTML = d.valor
+        linha.insertCell(3).innerHTML = valor
 
         let btn = document.createElement('button')
         btn.className = 'btn btn-danger btn-sm'
         btn.innerHTML = `<i class="fa-solid fa-xmark"></i>`
         btn.addEventListener('click', () => {
-            
+            bd.removerItem(d.id)
+            modal.removerDespesa()
+            $('#modal').modal('show')
         })
         linha.insertCell(4).appendChild(btn)
     })
+}
+
+function recuperarValorTotal() {
+    bd.recuperarTodosResgistros()
+
+    let valores = 0
+
+    todasDespesas.forEach((d) => {
+        valores += Number(d.valor)
+    })
+
+    if (valores > 10000) {
+        valores = ajustarValor(valores)
+        valorTotal.innerHTML = `${valores} 😨`
+    } else {
+        valores = ajustarValor(valores)
+        valorTotal.innerHTML = `${valores}`
+    }
+}
+
+function pesquisarValor() {
+    let ano = document.querySelector('#iano').value
+    let tipo = document.querySelector('#itipo').value
+    let mes = ''
+    let dia = ''
+    let descricao = ''
+    let valor = ''
+
+    if (ano == '' && tipo == '') {
+        modal.erroGravacao()
+        $('#modal').modal('show')
+    } else {
+        if (resultadoPesquisaValor.classList.contains('d-none')) {
+            resultadoPesquisaValor.classList.remove('d-none')
+        }
+
+        let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+        let despesaFiltrada = filtrar(despesa)
+
+        let valores = 0
+
+        despesaFiltrada.forEach((d) => {
+            valores += Number(d.valor)
+        })
+
+        tipo = ajustarTipo(tipo)
+        valores = ajustarValor(valores)
+
+        if (ano == '') {
+            filtroSelecionado.innerHTML = `em ${tipo}`
+        } else if (tipo == '') {
+            filtroSelecionado.innerHTML = `em ${ano}`
+        } else {
+            filtroSelecionado.innerHTML = `em ${tipo} em ${ano}`
+        }
+
+        valorGasto.innerHTML = `${valores}`
+    }
 }
 
 // FUNÇÕES SECUNDÁRIAS ----------------------------------
@@ -162,6 +268,11 @@ let limparDados = () => {
     document.querySelector('#itipo').value = ''
     document.querySelector('#idescricao').value = ''
     document.querySelector('#ivalor').value = ''
+}
+
+let recarregarPagina = () => {
+    modal.removerClasses()
+    window.location.reload()
 }
 
 function ajustarDiaMes(num) {
@@ -196,4 +307,40 @@ function ajustarValor(valor) {
     valor = Number(valor)
     valor = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
     return valor
+}
+
+function filtrar(despesa) {
+    let despesaFiltrada = todasDespesas
+
+    // ano
+    if (despesa.ano !== '') {
+        despesaFiltrada = despesaFiltrada.filter(d => d.ano == despesa.ano)
+    }
+    
+    // mes
+    if (despesa.mes !== '') {
+        despesaFiltrada = despesaFiltrada.filter(d => d.mes == despesa.mes)
+    }
+
+    // dia 
+    if (despesa.dia !== '') {
+        despesaFiltrada = despesaFiltrada.filter(d => d.dia == despesa.dia)
+    }
+
+    // tipo 
+    if (despesa.tipo !== '') {
+        despesaFiltrada = despesaFiltrada.filter(d => d.tipo == despesa.tipo)
+    }
+
+    // descricao
+    if (despesa.descricao !== '') {
+        despesaFiltrada = despesaFiltrada.filter(d => d.descricao == despesa.descricao)
+    }
+
+    // valor
+    if (despesa.valor !== '') {
+        despesaFiltrada = despesaFiltrada.filter(d => d.valor == despesa.valor)
+    }
+
+    return despesaFiltrada
 }
